a2a8295bdddfb3f72ba816823045e2ca2b6ce0be
ilia <kozulinilia@yandex.ru>
2025-12-21 09:53:21 +0300

    [BS-02] исправления глобальных переменных

--- Код.js
+++ Код.js
@@ -8,206 +8,210 @@ const BOX_ROW = 11;
 const THEME_ROW = 2;
 const LINK_ROW = 10;
 const GLOBAL = {};
+const FREE_STATUS = 'free';
 
 function doGet(e) {
-  return HtmlService
-    .createTemplateFromFile("index")
-    .evaluate()
-    .setTitle("Фильтры из таблицы");
+    return HtmlService
+        .createTemplateFromFile("index")
+        .evaluate()
+        .setTitle("Фильтры из таблицы");
 }
 
 function getFilteredData(filterTheme = false) {
-  filterStatus = 'free';
-  const ss = SpreadsheetApp.openById(TABLE_ID);
-  const sheet = ss.getSheetByName(BOOKS_SHEET);
+    loadEnvironment();
+    const ss = SpreadsheetApp.openById(GLOBAL.tableId);
+    const sheet = ss.getSheetByName(GLOBAL.booksSheet);
 
-  const data = sheet.getDataRange().getValues();
-  const rawHeader = data.shift();
-  const header = [
-    rawHeader[2],
-    rawHeader[5],
-    rawHeader[6],
-    rawHeader[7],
-    rawHeader[8],
-    rawHeader[10],
-    rawHeader[12]
-  ];
-  Logger.log(header)
-  const colTheme = 2;
-  const colStatus = 14;
+    const data = sheet.getDataRange().getValues();
+    const rawHeader = data.shift();
+    const header = [
+        rawHeader[2],
+        rawHeader[5],
+        rawHeader[6],
+        rawHeader[7],
+        rawHeader[8],
+        rawHeader[10],
+        rawHeader[12]
+    ];
 
-  const result = data.filter(row => {
-    const matchTheme = filterTheme ? row[colTheme].includes(filterTheme) : true;
-    const matchStatus = row[STATE_ROW] === "free";
+    const result = data.filter(row => {
+        const matchTheme = filterTheme ? row[THEME_ROW].includes(filterTheme) : true;
+        const matchStatus = row[STATE_ROW] === FREE_STATUS;
 
-    return matchTheme && matchStatus;
-  })
-    .map(row => [
-      row[2],
-      row[5],
-      row[6],
-      row[7],
-      row[8],
-      row[10],
-      row[12],
-    ]);
+        return matchTheme && matchStatus;
+    })
+        .map(row => [
+            row[2],
+            row[5],
+            row[6],
+            row[7],
+            row[8],
+            row[10],
+            row[12],
+        ]);
 
-  return { header, rows: result };
+    return {header, rows: result};
 }
 
 function getDropdownValues() {
-  const ss = SpreadsheetApp.openById(TABLE_ID);
-  const sheet = ss.getSheetByName(BOOKS_SHEET);
+    loadEnvironment();
+    const ss = SpreadsheetApp.openById(GLOBAL.tableId);
+    const sheet = ss.getSheetByName(GLOBAL.booksSheet);
 
-  const data = sheet.getDataRange().getValues();
-  var themes = new Map();
+    const data = sheet.getDataRange().getValues();
+    var themes = new Map();
 
-  data.filter(v => v[STATE_ROW] === 'free').forEach(item => {
-    var subThemes = item[THEME_ROW].split(', ');
-    subThemes.forEach(theme => themes.set(theme, 1));
-  })
-  const valuesTheme = [...new Set(Array.from(themes.keys()))];
+    data.filter(v => v[STATE_ROW] === 'free').forEach(item => {
+        var subThemes = item[THEME_ROW].split(', ');
+        subThemes.forEach(theme => themes.set(theme, 1));
+    })
+    const valuesTheme = [...new Set(Array.from(themes.keys()))];
 
-  return valuesTheme ;
+    return valuesTheme;
 }
 
 function addNewBook(id = null) {
-  const ss = SpreadsheetApp.openById(TABLE_ID);
-  const sheet = ss.getSheetByName(BOOKS_SHEET);
-  let newId = 0;
-  if (id === null) {
-    const data = sheet.getDataRange().getValues();
-    newId = 0.0;
-    data.forEach(row => {
-      if (Number(row[BOOK_ID_ROW])) {
-        newId = Math.max(row[BOOK_ID_ROW], newId);
-      }
-    })
-    newId++;
-  } else {
-    newId = id;
-  }
-  let row = createBlankRow(Math.ceil(newId));
-  sheet.appendRow(row);
+    loadEnvironment();
+    const ss = SpreadsheetApp.openById(GLOBAL.tableId);
+    const sheet = ss.getSheetByName(GLOBAL.booksSheet);
+    let newId = 0;
+    if (id === null) {
+        const data = sheet.getDataRange().getValues();
+        newId = 0.0;
+        data.forEach(row => {
+            if (Number(row[BOOK_ID_ROW])) {
+                newId = Math.max(row[BOOK_ID_ROW], newId);
+            }
+        })
+        newId++;
+    } else {
+        newId = id;
+    }
+    let row = createBlankRow(Math.ceil(newId));
+    sheet.appendRow(row);
 
-  return newId;
+    return newId;
 }
 
 function saveNewBook(book) {
-  book.link = getImageLinkFromFolder(book.id);
-  let newRow = createRow(book);
-  Logger.log(book);
-  replaceRowById(book.id, newRow);
+    book.link = getImageLinkFromFolder(book.id);
+    let newRow = createRow(book);
+    Logger.log(book);
+    replaceRowById(book.id, newRow);
 
-  return true;
+    return true;
 }
 
 function createBlankRow(id) {
-  const row = [];
-  for (let i = 0; i < 15; i++) {
-    if (Math.ceil(i) === BOOK_ID_ROW) {
-      row.push(id)
-    } else if (i === STATE_ROW) {
-      row.push('free')
-    } else {
-      row.push('')
+    const row = [];
+    for (let i = 0; i < 15; i++) {
+        if (Math.ceil(i) === BOOK_ID_ROW) {
+            row.push(id)
+        } else if (i === STATE_ROW) {
+            row.push('free')
+        } else {
+            row.push('')
+        }
     }
-  }
 
-  return row;
+    return row;
 }
 
 function createRow(param) {
-  const row = [];
-  for (let i = 0; i < 15; i++) {
-    switch (i) {
-      case THEME_ROW:
-        row.push(param.theme);
-        break;
-      case BOOK_ID_ROW:
-        row.push(param.id);
-        break;
-      case STATE_ROW:
-        row.push('free');
-        break;
-      case AUTHOR_ROW:
-        row.push(param.author);
-        break;
-      case NAME_ROW:
-        row.push(param.name);
-        break;
-      case YEAR_ROW:
-        row.push(param.year);
-        break;
-      case ANNOTATION_ROW:
-        row.push(param.annotation);
-        break;
-      case BOX_ROW:
-        row.push(param.boxNum);
-        break;
-      case LINK_ROW:
-        row.push(param.link);
-        break;
-      default:
-        row.push('');
+    const row = [];
+    for (let i = 0; i < 15; i++) {
+        switch (i) {
+            case THEME_ROW:
+                row.push(param.theme);
+                break;
+            case BOOK_ID_ROW:
+                row.push(param.id);
+                break;
+            case STATE_ROW:
+                row.push('free');
+                break;
+            case AUTHOR_ROW:
+                row.push(param.author);
+                break;
+            case NAME_ROW:
+                row.push(param.name);
+                break;
+            case YEAR_ROW:
+                row.push(param.year);
+                break;
+            case ANNOTATION_ROW:
+                row.push(param.annotation);
+                break;
+            case BOX_ROW:
+                row.push(param.boxNum);
+                break;
+            case LINK_ROW:
+                row.push(param.link);
+                break;
+            default:
+                row.push('');
+        }
     }
-  }
 
-  return row;
+    return row;
 }
 
 function replaceRowById(id, newRow) {
-  const ss = SpreadsheetApp.openById(TABLE_ID);
-  const sh = ss.getSheetByName(BOOKS_SHEET);
+    loadEnvironment();
+    const ss = SpreadsheetApp.openById(GLOBAL.tableId);
+    const sh = ss.getSheetByName(GLOBAL.booksSheet);
 
-  const data = sh.getDataRange().getValues();
+    const data = sh.getDataRange().getValues();
 
-  for (let i = 1; i < data.length; i++) {
-    if (Math.ceil(data[i][BOOK_ID_ROW]) === Math.ceil(id)) {
-      sh.getRange(i + 1, 1, 1, newRow.length).setValues([newRow]);
-      return;
+    for (let i = 1; i < data.length; i++) {
+        if (Math.ceil(data[i][BOOK_ID_ROW]) === Math.ceil(id)) {
+            sh.getRange(i + 1, 1, 1, newRow.length).setValues([newRow]);
+            return;
+        }
     }
-  }
 
-  throw new Error("Строка с ID не найдена");
+    throw new Error("Строка с ID не найдена");
 }
 
 function getImageLinkFromFolder(imageId) {
-  const folder = DriveApp.getFolderById(BOOK_IMAGE_DIR_ID);
+    loadEnvironment();
+    const folder = DriveApp.getFolderById(GLOBAL.imgDir_ID);
 
-  const files = folder.getFiles();
-  while (files.hasNext()) {
-    const file = files.next();
+    const files = folder.getFiles();
+    while (files.hasNext()) {
+        const file = files.next();
 
-    if (file.getName() === imageId + ".jpg") {
-      return file.getUrl();
+        if (file.getName() === imageId + ".jpg") {
+            return file.getUrl();
+        }
     }
-  }
-  throw new Error("Изображение не найдено");
+    throw new Error("Изображение не найдено");
 }
 
 function removeRowById(id) {
-  const ss = SpreadsheetApp.openById(TABLE_ID);
-  const sh = ss.getSheetByName(BOOKS_SHEET);
-  const data = sh.getDataRange().getValues();
+    loadEnvironment();
+    const ss = SpreadsheetApp.openById(GLOBAL.tableId);
+    const sh = ss.getSheetByName(GLOBAL.booksSheet);
+    const data = sh.getDataRange().getValues();
 
-  for (let i = 1; i < data.length; i++) {
-    if (Math.ceil(data[i][BOOK_ID_ROW]) === Math.ceil(id)) {
-      sh.deleteRow(i + 1);
-      return;
+    for (let i = 1; i < data.length; i++) {
+        if (Math.ceil(data[i][BOOK_ID_ROW]) === Math.ceil(id)) {
+            sh.deleteRow(i + 1);
+            return;
+        }
     }
-  }
 }
 
 function getFullThemes() {
-    const ss = SpreadsheetApp.openById(TABLE_ID);
-      const sheet = ss.getSheetByName(THEME_SHEET);
-      const data = sheet.getDataRange().getValues();
-      const themes = [];
-      for (let i = 1; i < data.length; i++) {
+    loadEnvironment();
+    const ss = SpreadsheetApp.openById(GLOBAL.tableId);
+    const sheet = ss.getSheetByName(GLOBAL.themesSheet);
+    const data = sheet.getDataRange().getValues();
+    const themes = [];
+    for (let i = 1; i < data.length; i++) {
         themes.push(data[i][1])
-      }
-      return themes;
+    }
+    return themes;
 }
 
 function loadEnvironment() {
